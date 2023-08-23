809ee89c2f9aed01275ceebf1c5192dabe1b130b
KuchinaMA <142692420+KuchinaMA@users.noreply.github.com>
2023-08-23 13:51:58 +0300

    Add files via upload
    добавила функции для решения линейного уравнения и для вывода корней

--- Kvadrnew.cpp
+++ Kvadrnew.cpp
@@ -5,8 +5,10 @@
 // TODO: gh
 
 // TODO: fix codestyle
-int solve_square (double a, double b, double c, double* x1, double* x2);
+int solve_square (double a, double b, double c, double *x1, double *x2);
+int solve_linear (double b, double c, double *x);
 int is_zero(double n);
+int print_solutions(int nRoots, double x1, double x2);
 
 // TODO: read about enums
 /*const int INF_ROOTS = -1; */
@@ -27,9 +29,11 @@ int main()
 
     double x1 = 0, x2 = 0;
     int nRoots = solve_square (a, b, c, &x1, &x2);
+    print_solutions(nRoots, x1, x2);
 
     // TODO: print_solutions
-    switch (nRoots)
+
+    /*switch (nRoots)
         {
         case ZERO_ROOTS: printf("This equation doesn't have any roots\n");
                 break;
@@ -46,34 +50,78 @@ int main()
         default: printf ("main(): ERROR: nRoots =  %d\n",
                          nRoots);
                  return 1;
-        }
+        }*/
 
     return 0;
     }
+
+ int print_solutions(int nRoots, double x1, double x2)
+     {
+     switch (nRoots)
+        {
+        case ZERO_ROOTS: printf("This equation doesn't have any roots\n");
+                return 0;
+                break;
+
+        case ONE_ROOT: printf("x = %lg\n", x1);
+                return 0;
+                break;
+
+        case TWO_ROOTS: printf("x1 = %lg, x2 = %lg\n", x1, x2);
+                return 0;
+                break;
+
+        case INF_ROOTS: printf("This equation has infinite number of roots\n");
+                return 0;
+                break;
+
+        default: printf ("main(): ERROR: nRoots =  %d\n",
+                         nRoots);
+                 return 1;
+        }
+    }
+
 
 int is_zero(double n)
     {
     return fabs(n) < EPSILON;
     }
 
+int solve_linear (double b, double c, double *x)
+    {
+    if (is_zero(b))
+        {
+        return (is_zero(c)) ? INF_ROOTS : ZERO_ROOTS;
+        }
+    else
+        {
+        *x = -c/b;
+        return ONE_ROOT;
+        }
+    }
 
 
 // linear equation ???
+
 int solve_square (double a, double b, double c, double *x1, double *x2)
     {
     if (is_zero(a))
         {
+        int ans = solve_linear (b, c, x1);
+        return ans;
+        }
+        /*{
         if (is_zero(b))
             {
             return (is_zero(c)) ? INF_ROOTS : ZERO_ROOTS;
             }
         else
             {
-            *x1 = -b/c;
+            *x1 = -c/b;
             // TODO: get rid of magic consts
             return ONE_ROOT;
             }
-        }
+        }*/
     else
         {
         double d = b*b - 4*a*c;
@@ -98,3 +146,5 @@ int solve_square (double a, double b, double c, double *x1, double *x2)
             }
         }
     }
+
+
